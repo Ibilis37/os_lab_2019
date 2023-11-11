25a2d82666f635f226db1372d01f13b1747ae21c
DianaK <115741678+Ibilis37@users.noreply.github.com>
2023-11-11 12:06:22 +0000

    Update 44 files

---
+++ lab1/src/script.sh
@@ -0,0 +1,5 @@
+./#!/bin/bash
+echo "$PwD"
+echo "$(date)"
+echo "$PATH"
+
--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,12 @@
 #include "revert_string.h"
-
-void RevertString(char *str)
+#include <string.h>
+void RevertString(char* str)
 {
-	// your code here
+        int len = strlen(str);
+        char var;
+        for(int i = 0; i<len/2; i++){
+		var = str[i];
+		str[i] = str[len-i-1];
+                str[len-i-1]=var;
+        }  
 }
-
